de1b87e8c58962f56e848c3c0f052af7686f77cc
PofMagicfingers <pof@pof.pm>
2014-06-18 19:19:06 +0200

    Adding French translation

---
+++ vendor/assets/javascripts/lang/summernote-fr-FR.js
@@ -0,0 +1,101 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+	'fr-FR': {
+	  font: {
+	    bold: 'Gras',
+	    italic: 'Italique',
+	    underline: 'Souligné',
+	    strike: 'Barré',
+	    clear: 'Suprimer la mise en forme',
+	    height: 'Hauteur de ligne',
+	    size: 'Taille'
+	  },
+	  image: {
+	    image: 'Image',
+	    insert: 'Insérer une image',
+	    resizeFull: 'Taille originale',
+	    resizeHalf: '50%',
+	    resizeQuarter: '25%',
+	    floatLeft: 'Aligner à gauche',
+	    floatRight: 'Aligner à droite',
+	    floatNone: 'Aucun alignement',
+	    dragImageHere: 'Glissez une image ici',
+	    selectFromFiles: 'Parcourir',
+	    url: 'URL de l'image'
+	  },
+	  link: {
+	    link: 'Lien',
+	    insert: 'Insérer un lien',
+	    unlink: 'Supprimer le lien',
+	    edit: 'Editer',
+	    textToDisplay: 'Texte à afficher',
+	    url: 'URL du lien'
+	  },
+	  video: {
+	    video: 'Vidéo',
+	    videoLink: 'Lien de la vidéo',
+	    insert: 'Insérer la vidéo',
+	    url: 'URL de la vidéo',
+	    providers: '(YouTube, Vimeo, Vine, Instagram, ou Dailymotion)'
+	  },
+	  table: {
+	    table: 'Tableau'
+	  },
+	  hr: {
+	    insert: 'Insérer une ligne horizontale'
+	  },
+	  style: {
+	    style: 'Style',
+	    normal: 'Normal',
+	    blockquote: 'Citation',
+	    pre: 'Code',
+	    h1: 'Titre 1',
+	    h2: 'Titre 2',
+	    h3: 'Titre 3',
+	    h4: 'Titre 4',
+	    h5: 'Titre 5',
+	    h6: 'Titre 6'
+	  },
+	  lists: {
+	    unordered: 'Liste à puce',
+	    ordered: 'Liste numérotée'
+	  },
+	  options: {
+	    help: 'Aide',
+	    fullscreen: 'Plein écran',
+	    codeview: 'Code source'
+	  },
+	  paragraph: {
+	    paragraph: 'Paragraphe',
+	    outdent: 'Diminuer le retrait',
+	    indent: 'Augmenter le retrait',
+	    left: 'Aligner à gauche',
+	    center: 'Aligner au center',
+	    right: 'Aligner à droite',
+	    justify: 'Justifier'
+	  },
+	  color: {
+	    recent: 'Couleur récente',
+	    more: 'Plus de couleurs',
+	    background: 'Arrière plan',
+	    foreground: 'Couleur du texte',
+	    transparent: 'Transparent',
+	    setTransparent: 'Set transparent',
+	    reset: 'Réinitialiser',
+	    resetToDefault: 'Par défaut'
+	  },
+	  shortcut: {
+	    shortcuts: 'Raccourcis',
+	    close: 'Fermer',
+	    textFormatting: 'Mise en forme',
+	    action: 'Action',
+	    paragraphFormatting: 'Mise en forme des paragraphes',
+	    documentStyle: 'Style du document'
+	  },
+	  history: {
+	    undo: 'Annuler',
+	    redo: 'Recommencer'
+	  }
+	}
+  });
+})(jQuery);
