ba1ba682b6e8cee032006d5fc25c801c00c5bdf0
Samur Araujo, PhD. <samuraraujo@gmail.com>
2014-08-12 20:21:16 -0300

    Create summernote-pt-BR.js
    Add pt-BR locale

---
+++ vendor/assets/javascripts/lang/summernote-pt-BR.js
@@ -0,0 +1,101 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+    'it-IT': {
+      font: {
+        bold: 'Negrito',
+        italic: 'Itálico',
+        underline: 'Subscrito',
+        strike: 'Linha',
+        clear: 'Limpa formatação',
+        height: 'Altura da linha',
+        size: 'Tamanho do caracter'
+      },
+      image: {
+        image: 'Imagem',
+        insert: 'Inserir Imagem',
+        resizeFull: 'Dimensão original',
+        resizeHalf: 'Redimensionar para 50%',
+        resizeQuarter: 'Redimensionar para 25%',
+        floatLeft: 'Alinhar na esquerda',
+        floatRight: 'Alinhar na direita',
+        floatNone: 'Sem alinhamento',
+        dragImageHere: 'Arraste sua imagem aqui',
+        selectFromFiles: 'Seleciona de arquivo',
+        url: 'URL da imagem'
+      },
+      link: {
+        link: 'Link',
+        insert: 'Inserir link',
+        unlink: 'Eliminar link',
+        edit: 'Modificar link',
+        textToDisplay: 'Texto do link',
+        url: 'URL do link'
+      },
+      video: {
+        video: 'Video',
+        videoLink: 'Link do Video',
+        insert: 'Inserir Video',
+        url: 'URL do Video',
+        providers: '(YouTube, Vimeo, Vine, Instagram o DailyMotion)'
+      },
+      table: {
+        table: 'Tabela'
+      },
+      hr: {
+        insert: 'Linha de separação'
+      },
+      style: {
+        style: 'Estilo',
+        normal: 'Normal',
+        blockquote: 'Citação',
+        pre: 'Codigo',
+        h1: 'Titulo 1',
+        h2: 'Titulo 2',
+        h3: 'Titulo 3',
+        h4: 'Titulo 4',
+        h5: 'Titulo 5',
+        h6: 'Titulo 6'
+      },
+      lists: {
+        unordered: 'Lista sem ordem',
+        ordered: 'Lista numerada'
+      },
+      options: {
+        help: 'Ajuda',
+        fullscreen: 'Tela cheia',
+        codeview: 'Visualizar código html'
+      },
+      paragraph: {
+        paragraph: 'Parágrafo',
+        outdent: 'De-identar',
+        indent: 'Identar',
+        left: 'Alinhar à esquerda',
+        center: 'Centro',
+        right: 'Alinhar à direita',
+        justify: 'Justificado'
+      },
+      color: {
+        recent: 'Última cor utilizada',
+        more: 'Outra cor',
+        background: 'Cor de fundo',
+        foreground: 'Color do texto',
+        transparent: 'Trasparente',
+        setTransparent: 'Trasparente',
+        reset: 'Resetar',
+        resetToDefault: 'Resetar cor'
+      },
+      shortcut: {
+        shortcuts: 'Atalho',
+        close: 'Fechar',
+        textFormatting: 'Formatar texto',
+        action: 'Ação',
+        paragraphFormatting: 'Formatar paragrafo',
+        documentStyle: 'Estilo'
+      },
+      history: {
+        undo: 'Desfazer',
+        redo: 'Refazer'
+      }
+    }
+  });
+})(jQuery);
