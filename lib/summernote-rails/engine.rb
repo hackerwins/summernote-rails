3cbc188c4f9e5b51cf8efa563b796bd4fea207f6
Mohamed Ziata <wakematta@gmail.com>
2017-04-07 14:44:32 +0200

    Do not use Sprockets::Rails if is not defined
    Rails 3.2 do not have `Sprockets::Rails`.

--- lib/summernote-rails/engine.rb
+++ lib/summernote-rails/engine.rb
@@ -3,12 +3,15 @@ module SummernoteRails
     class Engine < ::Rails::Engine
       initializer 'summernote-rails.assets.precompile' do |app|
         app.config.assets.paths << root.join('vendor', 'assets', 'fonts')
-        # fix for: https://github.com/twbs/bootstrap-sass/issues/960
-        # regex no longer supported by assets.precompile
-        # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
-        # and automatically precompiles the referenced assets.
-        unless Sprockets::Rails::VERSION.starts_with?('3')
-          app.config.assets.precompile << /\.(?:eot|woff|ttf)$/
+        
+        if defined? Sprockets::Rails
+          # fix for: https://github.com/twbs/bootstrap-sass/issues/960
+          # regex no longer supported by assets.precompile
+          # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
+          # and automatically precompiles the referenced assets.
+          unless Sprockets::Rails::VERSION.starts_with?('3')
+            app.config.assets.precompile << /\.(?:eot|woff|ttf)$/
+          end
         end
       end
     end
