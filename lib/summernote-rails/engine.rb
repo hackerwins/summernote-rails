4427fc214bda4b663ef66470e1d60ace953fa07d
Mohamed Ziata <wakematta@gmail.com>
2017-05-06 01:09:57 +0200

    Do not use Sprockets::Rails if is not defined (#63)
    * Do not use Sprockets::Rails if is not defined
    
    Rails 3.2 do not have `Sprockets::Rails`.
    
    * Make the code more understandable

--- lib/summernote-rails/engine.rb
+++ lib/summernote-rails/engine.rb
@@ -3,13 +3,20 @@ module SummernoteRails
     class Engine < ::Rails::Engine
       initializer 'summernote-rails.assets.precompile' do |app|
         app.config.assets.paths << root.join('vendor', 'assets', 'fonts')
+
         # fix for: https://github.com/twbs/bootstrap-sass/issues/960
         # regex no longer supported by assets.precompile
         # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
         # and automatically precompiles the referenced assets.
-        unless Sprockets::Rails::VERSION.starts_with?('3')
+        if old_sprocket?
           app.config.assets.precompile << /\.(?:eot|woff|ttf)$/
         end
+      end
+
+      private
+
+      def old_sprocket?
+        defined?(Sprockets::Rails) && !Sprockets::Rails::VERSION.starts_with?('3')
       end
     end
   end
